6d590a5492e45c6b2a782ffc8247f2e0f586eef0
Renato H. Osaka <renato.osaka@saipos.com>
2021-07-04 18:31:35 -0300

    Image optimization domains

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ module.exports = {
     includePaths: [path.join(__dirname, 'src', 'styles')],
   },  
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'd6yngcq6k5.execute-api.us-east-1.amazonaws.com', 'd6yngcq6k5.execute-api.us-east-1.amazonaws.com/prod'],
   },
 }
